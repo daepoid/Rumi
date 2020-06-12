061ae29bbf46d0514045cf5998bf4910b74f2afd
tkdgp35 <35tkdgp@gmail.com>
2020-06-13 02:13:00 +0900

    6.13-1
    Master client만이 PLAYERS 리스트를 생성하는 것에 맞춰 Game 씬의 플레이어 카드 갯수 텍스트를 띄우는 코드 수정

--- Assets/Scripts/Game/PlayerTextManager.cs
+++ Assets/Scripts/Game/PlayerTextManager.cs
@@ -13,7 +13,6 @@ public class PlayerTextManager : MonoBehaviourPunCallbacks
     private Photon.Realtime.Player[] currentPlayersInRoom;          //현재 룸 플레이어 정보
     private Photon.Realtime.Player[] playersInRoomOthersLeft;       //바로 전 상태 룸 플레이어 정보
     private int localPlayerIndex;
-    private List<Player> playersInfo;
     
     void Start()
     {
@@ -26,27 +25,48 @@ public class PlayerTextManager : MonoBehaviourPunCallbacks
     //게임 시작버튼 눌렀을 때
     public void ClickStartButton()
     {
-        photonView.RPC("PrintCardNumberText", RpcTarget.All);
-    }
-    
-    [PunRPC]
-    void PrintCardNumberText()
-    {
-        //현재 플레이어들 정보 가져오기 이미 있
-        playersInfo = MyGameManager.PLAYERS;
-
-        //player0 카드 갯수 출력
-        Player player = playersInfo.Find(x => x.NickName == currentPlayersInRoom[localPlayerIndex].NickName);
-        PlayerText[1].text = player.cards.Length.ToString();
-
-        //다른 플레이어 카드 갯수 출력
-        for (int i = 0; i < PhotonNetwork.PlayerListOthers.Length; i++)
+        if (PhotonNetwork.IsMasterClient)
         {
-            player = playersInfo.Find(x => x.NickName == PhotonNetwork.PlayerListOthers[i].NickName);
-            PlayerText[2 * i + 1].text = player.cards.Length.ToString();
+            getCardNumberList();
+            photonView.RPC("StartCardNumberText", RpcTarget.All, getCardNumberList());
+
         }
     }
 
+    //카드 갯수 리스트 얻어오기
+    int[] getCardNumberList()
+    {
+        int[] cardlist = { 0, 0, 0, 0 };
+
+        for(int i = 0; i < currentPlayersInRoom.Length; i++)
+        {
+            cardlist[i] = MyGameManager.PLAYERS[i].card_num;
+        }
+
+        return cardlist;
+    }
+
+
+    [PunRPC]
+    void StartCardNumberText(int[] playersCardNumber)
+    {
+        int passLocal = 0;
+        //player0 카드 갯수 출력
+        PlayerText[1].text = playersCardNumber[localPlayerIndex].ToString();
+
+        //다른 플레이어 카드 갯수 출력
+        for(int i = 0; i < currentPlayersInRoom.Length; i++)
+        {
+            if (i == localPlayerIndex)
+            {
+                PlayerText[1].text = playersCardNumber[localPlayerIndex].ToString();
+                continue;
+            }
+            PlayerText[2 * (i - passLocal) + 1].text = playersCardNumber[i].ToString();
+        }
+    }
+    
+    /*
     //게임 중 확인버튼 늘렀을 때
     public void ClickOkButton()
     {
@@ -72,6 +92,7 @@ public class PlayerTextManager : MonoBehaviourPunCallbacks
             PlayerText[1].text = player.cards.Length.ToString();
         }
     }
+    */
     
     //텍스트 출력
     void printNameText()
