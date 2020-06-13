4bfcdeea84bdf2c3faeb2c1faafd027631ae6234
tkdgp35 <35tkdgp@gmail.com>
2020-06-13 18:10:14 +0900

    SingUp 기능 추가

--- Assets/Scripts/SignIn/AuthManager.cs
+++ Assets/Scripts/SignIn/AuthManager.cs
@@ -10,9 +10,19 @@ public class AuthManager : MonoBehaviour
     public bool IsFirebaseReady { get; private set; }
     public bool IsSignInOnProgress { get; private set; }
 
+    //Main Panel Objects
     public InputField emailField;
     public InputField passwordField;
-    public Button signInButton;
+    public Button signInReqButton;
+    public Button signUpReqButton;
+    public GameObject signUpPannel;
+
+    //Sign Panel Objects
+    public InputField signUpEmailField;
+    public InputField signUpPasswordField;
+    public Button backButton;
+    public Button signUpButton;
+    public Text WarningText;
 
     public static FirebaseApp firebaseApp;
     public static FirebaseAuth firebaseAuth;
@@ -22,7 +32,8 @@ public class AuthManager : MonoBehaviour
     public void Start()
     {
         //버튼 비활성화
-        signInButton.interactable = false;
+        signInReqButton.interactable = false;
+        signUpReqButton.interactable = false;
         
         //앱또는 환경이 파이어베이스 구동할 수 있는지 확인
         //안될 경우 dependency를 fix할 수 있다면 자동 fix
@@ -44,12 +55,20 @@ public class AuthManager : MonoBehaviour
                 firebaseAuth = FirebaseAuth.DefaultInstance;
             }
 
-            signInButton.interactable = IsFirebaseReady;
+            signInReqButton.interactable = IsFirebaseReady;
+            signUpReqButton.interactable = IsFirebaseReady;
         }
         );
     }
+
+    //main panel에서 신규 가입 버튼을 누르면 실행
+    public void clickSignUpRequestButton()
+    {
+        signUpPannel.SetActive(true);
+    }
     
-    public void SignIn()
+    //main panel에서 로그인 버튼을 누르면 실행
+    public void clickSignInRequestButton()
     {
         //파이어베이스가 준비되지 않았거나 로그인 중이거나 유저정보가 이미 있다면
         if(!IsFirebaseReady || IsSignInOnProgress || User!= null)
@@ -58,14 +77,14 @@ public class AuthManager : MonoBehaviour
         }
 
         IsSignInOnProgress = true;
-        signInButton.interactable = false;
+        signInReqButton.interactable = false;
         
         firebaseAuth.SignInWithEmailAndPasswordAsync(emailField.text, passwordField.text).ContinueWithOnMainThread((task) =>
         {
             //로그인 시도 후 실행
             Debug.Log($"Sign in status:{task.Status}");
             IsSignInOnProgress = false;
-            signInButton.interactable = true;
+            signInReqButton.interactable = true;
 
             if (task.IsFaulted)
             {
@@ -81,6 +100,39 @@ public class AuthManager : MonoBehaviour
                 Debug.Log(User.Email);
                 SceneManager.LoadScene("Scenes/Lobby");
             }
+        });
+    }
+ 
+    //Sign Up Pannel에서 뒤로가기 버튼 클릭 시 실행
+    public void clickBackButton()
+    {
+        signUpPannel.SetActive(false);
+    }
+
+    //Sign Up Pannel에서 가입하기 버튼 클릭 시 실행
+    public void clickSignUpButton()
+    {
+        backButton.interactable = false;
+        signUpButton.interactable = false;
+
+        firebaseAuth.CreateUserWithEmailAndPasswordAsync(signUpEmailField.text, signUpPasswordField.text).ContinueWithOnMainThread((task) =>
+        {
+            if (task.IsFaulted)
+            {
+                WarningText.text = "이미 존재하거나 잘못된 이메일입니다";
+                Debug.LogError("실패?");
+            }
+            else if (task.IsCanceled)
+            {
+                Debug.LogError("Sign up Canceled");
+            }
+            else
+            {
+                signUpPannel.SetActive(false);
+                return;
+            }
+            backButton.interactable = true;
+            signUpButton.interactable = true;
         });
     }
 }
